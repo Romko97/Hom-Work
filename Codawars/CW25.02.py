6b0015c14538d57e140b40e243f2799e4fd27dc6
Roman <Elgoog6651@gmail.com>
2020-03-07 01:39:26 +0200

    25.02and27.02

--- Codawars/CW25.02.py
+++ Codawars/CW25.02.py
@@ -11,8 +11,38 @@
 # whenever it does not contain any lowercase letter
 # so any string containing no letters at all is trivially
 # considered to be in ALL CAPS.
-
+'''
 def is_uppercase(inp):
-    return inp.isupper()
+    if inp.isupper(): return True 
+    return False
 
 is_uppercase("I sgksjfngAM DONALD")
+'''
+# "Sort My Textbooks"
+# Description:
+# HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+
+# The sorting should NOT be case sensitive
+"""
+def sorter(textbooks):
+    return sorted(textbooks, key=lambda e: e.lower())
+"""
+
+# You're re-designing a blog and the blog's posts have the
+# following format for showing the date and time a post was made:
+
+# Weekday Month Day, time e.g., Friday May 2, 7pm
+
+# You're running out of screen real estate, and on some pages
+# you want to display a shorter format, Weekday Month Day that omits the time.
+
+# Write a function, shortenToDate, that takes the Website
+# date/time in its original string format, and returns the shortened format.
+
+# Assume shortenToDate's input will always be a string, e.g.
+# "Friday May 2, 7pm". Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".
+
+"""
+def shorten_to_date(long_date):
+    return long_date.split(',')[0]
+"""
---
+++ Codawars/CW27.02.py
@@ -0,0 +1,43 @@
+# Regular Ball Super Ball
+# Create a class Ball.
+# Ball objects should accept one argument for "ball type" when instantiated.
+# If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+"""
+class Ball(object):
+    def __init__(self, ball_type="regular"):
+        self.ball_type = ball_type
+"""
+
+# Color Ghost
+# Create a class Ghost
+# Ghost objects are instantiated without any arguments.
+# Ghost objects are given a random color attribute of white" or
+# "yellow" or "purple" or "red" when instantiated
+"""
+import random
+class Ghost(object):
+  colors = ["white", "yellow", "purple", "red"]
+  def __init__(self):
+    self.color = random.choice(Ghost.colors)"""
+
+
+# According to the creation myths of the Abrahamic religions,
+# Adam and Eve were the first Humans to wander the Earth.
+# You have to do God's job. The creation method must return an array
+# of length 2 containing objects (representing Adam and Eve).
+# The first object in the array should be an instance of the class Man.
+# The second should be an instance of the class Woman. Both objects have
+# to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
+
+
+
+
+# Classy Classes
+# Basic Classes, this kata is mainly aimed at the new JS
+# ES6 Update introducing classes
+# Task
+# Your task is to complete this Class, the Person class has
+# been created. You must fill in the Constructor method to
+# accept a name as string and an age as number, complete the
+# get Info property and getInfo method/Info getter which should return
